0b6d0f1706ceeac5a72d2f50c578e20e4cdd4d61
Yash <yashn49@gmail.com>
2020-10-31 09:43:05 +0530

    attendance

--- Employee.java
+++ Employee.java
@@ -1,9 +1,19 @@
-import java.util.Scanner;
 import java.util.*;
 public class Employee
 {
  public static void main(String[] args)
  {
   System.out.println("Welcome to Employee Wage Computation Problem");
+  Random r=new Random();
+  int random=r.nextInt(2);
+  System.out.println(random);
+  if(random==1)
+  {
+   System.out.println("Employee is present");
+  }
+  else
+  {
+   System.out.println("Employee is absent");
+  }
  }
 }
